aee1e1bffdd8b113c2534e6419ae6c4a43344b39
ErickExploer <152933429+ErickExploer@users.noreply.github.com>
2024-12-01 19:18:29 -0500

    Update ValidarToken.py

--- TABLA-TOKENS_ACCESO/ValidarToken.py
+++ TABLA-TOKENS_ACCESO/ValidarToken.py
@@ -1,37 +1,33 @@
 import boto3
 from datetime import datetime
-import json
 
 def lambda_handler(event, context):
-    body = event.get('body')
-    if body is None:
-        return {'statusCode': 400, 'body': 'Solicitud inválida. Falta el cuerpo de la solicitud.'}
-
-    if isinstance(body, str):
-        try:
-            body = json.loads(body)
-        except json.JSONDecodeError:
-            return {'statusCode': 400, 'body': 'El formato del JSON es incorrecto.'}
-
-    token = body.get('token')
-    if not token:
-        return {'statusCode': 400, 'body': 'Solicitud inválida. Falta el campo "token".'}
-    
+    # Entrada (json)
+    token = event['token']
+    # Proceso
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('TABLA-TOKENS_ACCESO')
-
-    response = table.get_item(Key={'token': token})
-
+    table = dynamodb.Table('t_tokens_acceso')
+    response = table.get_item(
+        Key={
+            'token': token
+        }
+    )
     if 'Item' not in response:
-        return {'statusCode': 403, 'body': 'Token no existe'}
+        return {
+            'statusCode': 403,
+            'body': 'Token no existe'
+        }
+    else:
+        expires = response['Item']['expires']
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if now > expires:
+            return {
+                'statusCode': 403,
+                'body': 'Token expirado'
+            }
     
-    expires = response['Item']['expires']
-    if datetime.now().strftime('%Y-%m-%d %H:%M:%S') > expires:
-        return {'statusCode': 403, 'body': 'Token expirado'}
-
+    # Salida (json)
     return {
         'statusCode': 200,
-        'body': {
-            'usuario_id': response['Item']['usuario_id']
-        }
+        'body': 'Token válido'
     }
